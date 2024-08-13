0548deadf96b100fa236b4767dc5b08f43f5f3d8
@siti.nuraeni <sitinuraen20@gmail.com>
2024-08-13 19:36:53 +0700

    upload hasil test fe

---
+++ resources/views/index.php
@@ -0,0 +1,403 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Green Haven Project</title>
+    <link rel="stylesheet" href="assets/sass/main.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="assets/img/logo.png" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">About Green Haven</a></li>
+                    <li><a href="#">Event Details</a></li>
+                    <li><a href="#">Our Sponsors</a></li>
+                    <li><a href="#">Leaderboard</a></li>
+                </ul>
+            </nav>
+            <div class="cta">
+                <button href="#" class="btn">Plant a Mangrove</button>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <section class="hero">
+            <div class="container">
+                <div class="hero-content">
+                    <div class="cta">
+                        <button href="#" class="btn">Green Horizons Await, Join Us in Planting 10,000 Mangroves!</button>
+                    </div>
+                    <h1>Green Horizons Await, Join Us in <br> Planting 10,000 Mangroves!</h1>
+                    <div class="progress-card">
+                        <div class="progress-info">
+                            <h2>5,690 / 10,000 Pohon</h2>
+                            <a href="#" class="btn">Support Our Mission</a>
+                        </div>
+                        <div class="progress-bar">
+                            <div class="progress" style="width: 50%;"></div>
+                        </div>
+                    </div>
+                    <div class="rectangle">
+                        <img src="assets/img/Rectangle 1.png" alt="" width="850">
+                    </div>
+                </div>
+
+                <div class="leaderboard">
+                    <h2>Leaderboard</h2>
+                    <div class="tabs">
+                        <button class="tab-button active">Most Donation</button>
+                        <button class="tab-button">Most Recent</button>
+                    </div>
+                    <ul class="leader-list">
+                        <li>
+                            <div class="rank1">01</div>
+                            <img src="assets/img/planet-earth 1.png" alt="Mangrove Mover Badge">
+                            <div class="details">
+                                <div class="left-side">
+                                    <span class="badge-title">Mangrove Maven</span>
+                                    <span class="name">Budi Hartanto</span>
+                                </div>
+                                <div class="trees">
+                                    <img src="assets/img/mangrove.png" alt="Mangrove Icon">350 Pohon
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="rank1">02</div>
+                            <img src="assets/img/planet-earth 1.png" alt="Mangrove Mover Badge">
+                            <div class="details">
+                                <div class="left-side">
+                                    <span class="badge-title">Mangrove Maven</span>
+                                    <span class="name">Dewi Sartika</span>
+                                </div>
+                                <div class="trees">
+                                    <img src="assets/img/mangrove.png" alt="Mangrove Icon">350 Pohon
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="rank1">03</div>
+                            <img src="assets/img/planet-earth 1.png" alt="Mangrove Mover Badge">
+                            <div class="details">
+                                <div class="left-side">
+                                    <span class="badge-title">Mangrove Maven</span>
+                                    <span class="name">Eko Wahyudi</span>
+                                </div>
+                                <div class="trees">
+                                    <img src="assets/img/mangrove.png" alt="Mangrove Icon">350 Pohon
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="rank2">04</div>
+                            <img src="assets/img/planet-earth 2.png" alt="Guardian of the Grove Badge">
+                            <div class="details">
+                                <div class="left-side">
+                                    <span class="badge-title">Guardian of the Grave</span>
+                                    <span class="name">Rina Kartika</span>
+                                </div>
+                                <div class="trees">
+                                    <img src="assets/img/mangrove.png" alt="Mangrove Icon">350 Pohon
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="rank2">05</div>
+                            <img src="assets/img/planet-earth 2.png" alt="Guardian of the Grove Badge">
+                            <div class="details">
+                                <div class="left-side">
+                                    <span class="badge-title">Guardian of the Grave</span>
+                                    <span class="name">Agud Prasetyo</span>
+                                </div>
+                                <div class="trees">
+                                    <img src="assets/img/mangrove.png" alt="Mangrove Icon">350 Pohon
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="rank3">06</div>
+                            <img src="assets/img/planet-earth 3.png" alt="Mangrove Master Badge">
+                            <div class="details">
+                                <div class="left-side">
+                                    <span class="badge-title">Mangrove Master</span>
+                                    <span class="name">Irfan Setiawan</span>
+                                </div>
+                                <div class="trees">
+                                    <img src="assets/img/mangrove.png" alt="Mangrove Icon">350 Pohon
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section class="sponsors">
+            <div class="container">
+                <img src="assets/img/jala.png" alt="Jala">
+                <img src="assets/img/efish.png" alt="eFishery">
+                <img src="assets/img/azarine.png" alt="Azarine">
+                <img src="assets/img/kompas.png" alt="Kompas">
+                <img src="assets/img/tribun.png" alt="Tribunnews">
+                <img src="assets/img/jawa.png" alt="Jawa Pos">
+            </div>
+        </section>
+
+        <section class="about-green-haven">
+            <div class="about-container">
+                <div class="logos">
+                    <img src="assets/img/eastjava.png" alt="Logo 1">
+                    <img src="assets/img/Polygon.png" alt="Logo 2">
+                </div>
+                <h2>About Green Haven Project</h2>
+                <p>The Green Haven Project - Mangrove aims to restore coastal ecosystems and raise awareness by planting mangroves and educating participants about the importance of these areas. This six-hour event will involve JCI members, local farmers, and students, focusing on combating beach erosion and promoting ESG values.</p>
+                <div class="image-comparison">
+                    <div class="before">
+                        <img src="assets/img/img2.png" alt="Before mangroves were planted">
+                        <p>Before mangroves were planted</p>
+                    </div>
+                    <div class="center-arrow">
+                        <div class="arrow-circle">
+                            <img src="assets/img/arrow.png" alt="Arrow">
+                            <p class="transform-text">TRANSFORMATION</p>
+                        </div>
+                    </div>
+                    <div class="after">
+                        <img src="assets/img/img1.png" alt="After mangroves were planted">
+                        <p>After mangroves were planted</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="event-summary">
+            <div class="event-container">
+                <h2>Event Summary</h2>
+                <p class="event-description">
+                    We give you a brief overview of the event to give you a glimpse of what we’re doing in the Green Haven Project,
+                    download the detailed PDF to see how you can get involved
+                </p>
+                <div class="event-card">
+                    <div class="event-details">
+                        <div class="event-info">
+                            <h3>26/04/2024</h3>
+                            <h4>08:00 - 11:00 WIB</h4>
+                            <p>
+                                Join the impactful journey of planting mangroves, where you’ll learn, collaborate with locals,
+                                earn recognition, and share your story through media coverage
+                            </p>
+                            <div class="btn-event">
+                                <button href="#" class="btn">View Event Details</button>
+                            </div>
+                        </div>
+                        <div class="event-image">
+                            <img src="assets/img/event.png" alt="Event activities">
+                        </div>
+                    </div>
+                </div>
+                <div class="event-steps">
+                    <div class="step active">
+                        <h4>01</h4>
+                        <p>Introducing the beauty of mangroves on a journey to the planting site.</p>
+                    </div>
+                    <div class="step">
+                        <h4>02</h4>
+                        <p>Working together with local farmers while planting each mangrove</p>
+                    </div>
+                    <div class="step">
+                        <h4>03</h4>
+                        <p>Exploring the wonders of mangrove ecotourism, an educational adventure.</p>
+                    </div>
+                    <div class="step">
+                        <h4>04</h4>
+                        <p>Receive a plaque & badge, a mark of honor for every tree you plant.</p>
+                    </div>
+                    <div class="step">
+                        <h4>05</h4>
+                        <p>Coverage & documentation by news media, share your story of change!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="sponsorship-packages">
+            <h2>Sponsorship Packages</h2>
+            <p>
+                Our sponsorship package offers branding, promotional opportunities, and visibility at our event, ideal for enhancing company presence and community engagement.
+            </p>
+
+            <section class="personal-sponsorship">
+                <h3>Personal Sponsorship Package</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Membership Tier</th>
+                            <th>Min. Seeds</th>
+                            <th>Min. Buy</th>
+                            <th>Benefits</th>
+                            <th>Donors</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Seedling Scout</td>
+                            <td>5 Seeds</td>
+                            <td>IDR 75K</td>
+                            <td>Badge</td>
+                            <td>4,900 Donors</td>
+                            <td><button>Support Our Mission</button></td>
+                        </tr>
+                        <tr>
+                            <td>Sapling Savior</td>
+                            <td>10 Seeds</td>
+                            <td>IDR 150K</td>
+                            <td>Badge + Pin</td>
+                            <td>4,300 Donors</td>
+                            <td><button>Support Our Mission</button></td>
+                        </tr>
+                        <tr>
+                            <td>Tree Titan</td>
+                            <td>20 Seeds</td>
+                            <td>IDR 300K</td>
+                            <td>Badge + Pin</td>
+                            <td>15,000 Donors</td>
+                            <td><button>Support Our Mission</button></td>
+                        </tr>
+                        <tr>
+                            <td>Mangrove Master</td>
+                            <td>50 Seeds</td>
+                            <td>IDR 750K</td>
+                            <td>Badge + Pin + Bag</td>
+                            <td>5,000 Donors</td>
+                            <td><button>Support Our Mission</button></td>
+                        </tr>
+                        <tr>
+                            <td>Guardian Of The Grove</td>
+                            <td>100 Seeds</td>
+                            <td>IDR 1,500K</td>
+                            <td>Badge + Pin + Bag</td>
+                            <td>300 Donors</td>
+                            <td><button>Support Our Mission</button></td>
+                        </tr>
+                        <tr>
+                            <td>Mangrove Maven</td>
+                            <td>200 Seeds</td>
+                            <td>IDR 3,000K</td>
+                            <td>Badge + Pin + Bag</td>
+                            <td>10 Donors</td>
+                            <td><button>Support Our Mission</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+
+            <div class="corporate-sponsorship">
+                <h3>Corporate Sponsorship Package</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Benefits</th>
+                            <th>Eco Trailblazers</th>
+                            <th>Green Innovators</th>
+                            <th>Eco Vanguard</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Price</td>
+                            <td>15 Million IDR</td>
+                            <td>30 Million IDR</td>
+                            <td>45 Million IDR</td>
+                        </tr>
+                        <tr>
+                            <td>CO<sub>2</sub> Sequestration</td>
+                            <td>22.5 ton/year</td>
+                            <td>45 ton/year</td>
+                            <td>67.5 ton/year</td>
+                        </tr>
+                        <tr>
+                            <td>Number of Trees</td>
+                            <td>1000 Trees</td>
+                            <td>2000 Trees</td>
+                            <td>3000 Trees</td>
+                        </tr>
+                        <tr>
+                            <td>Logo on JCI Banner</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                        </tr>
+                        <tr>
+                            <td>Logo on Communal Stainless Plate</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                        </tr>
+                        <tr>
+                            <td>Considered as Main Sponsor</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                        </tr>
+                        <tr>
+                            <td>Dedicated Stainless Plate</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                        </tr>
+                        <tr>
+                            <td>Report Update on Planted Trees</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                        </tr>
+                        <tr>
+                            <td>Logo on Event Clothings</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                            <td>✔️</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="donate-buttons">
+                    <button>Make a Donation</button>
+                    <button>Support Our Mission</button>
+                    <button>Support Our Mission</button>
+                    <button>Support Our Mission</button>
+                </div>
+            </div>
+        </section>
+
+        <section class="donate-section">
+            <div class="donate-content">
+                <h2>Donate Today and be a Guardian of the Green Belt.</h2>
+                <p>Join us in our mission to rejuvenate and protect our shorelines by donating to our mangrove planting project.</p>
+                <button>Support Our Mission</button>
+            </div>
+
+            <div class="donate-images">
+                <img src="assets/img/Frame 88.png" alt="">
+                <img src="assets/img/Frame 89.png" alt="">
+                <img src="assets/img/Frame 90.png" alt="">
+                <img src="assets/img/Frame 91.png" alt="">
+                <img src="assets/img/Frame 92.png" alt="">
+            </div>
+        </section>
+
+        <footer>
+            <div class="footer-bottom">
+                <p>Nama yang Mengerjakan Test: Siti Nuraeni</p>
+            </div>
+        </footer>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
